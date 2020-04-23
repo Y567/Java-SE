2a2cc5321519f262df4f54721ea99d0a30483f4c
Y567 <1592221415@qq.com>
2020-04-23 19:39:49 +0800

    新增了map保存参数的使用

--- mybatis-review/mybatis-test/src/main/java/com/gyy/dao/UserMapper.java
+++ mybatis-review/mybatis-test/src/main/java/com/gyy/dao/UserMapper.java
@@ -3,6 +3,7 @@ package com.gyy.dao;
 import com.gyy.domain.User;
 
 import java.util.List;
+import java.util.Map;
 
 public interface UserMapper {
     /**
@@ -10,4 +11,39 @@ public interface UserMapper {
      * @return
      */
     List<User> getUserList();
+
+    /**
+     * 按照id查询用户名
+     * @param id  id
+     * @return  User
+     */
+    User findUserById(int id);
+
+    /**
+     * 插入一个用户
+     * @param user user
+     * @return  返回影响的行数
+     */
+    void insertUser(User user);
+
+    /**
+     * 更新指定的用户
+     * @param user user
+     */
+    void updateUser(User user);
+
+    /**
+     * 按照id进行用户删除
+     * @param id id
+     */
+    void deleteUser(int id);
+
+    /**
+     * 利用map保存查询条件
+     * @param map map
+     * @return 返回用户数据
+     */
+    List<User> getUserByMap(Map<String,Object> map);
+
+    List<User> getUserByLike(String username);
 }
