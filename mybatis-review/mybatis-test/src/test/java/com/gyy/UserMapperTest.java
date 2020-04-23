583f0ab46e0ff14e389431a5c44f1a167cdbc144
Y567 <1592221415@qq.com>
2020-04-23 19:41:25 +0800

    新增了map使用的测试与模糊查询

--- mybatis-review/mybatis-test/src/test/java/com/gyy/UserMapperTest.java
+++ mybatis-review/mybatis-test/src/test/java/com/gyy/UserMapperTest.java
@@ -6,7 +6,9 @@ import com.gyy.utils.MyBatisUtil;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class UserMapperTest {
 
@@ -25,5 +27,98 @@ public class UserMapperTest {
         //关闭
         sqlSession.close();
         //fa方式二
+    }
+
+    @Test
+    public void test2() {
+        SqlSession sqlSession = MyBatisUtil.getSqlSession();
+
+        //操作数据库的两种方式
+        //方式一
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+        userMapper.insertUser(new User(4,"阿狗4号","cqc112233"));
+
+        //增删改需要提交事务
+        sqlSession.commit();
+
+        //关闭
+        sqlSession.close();
+    }
+
+    @Test
+    public void test3() {
+        SqlSession sqlSession = MyBatisUtil.getSqlSession();
+
+        //操作数据库的两种方式
+        //方式一
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+        User user = userMapper.findUserById(3);
+
+        System.out.println(user);
+        //关闭
+        sqlSession.close();
+    }
+
+    @Test
+    public void test4() {
+        SqlSession sqlSession = MyBatisUtil.getSqlSession();
+
+        //操作数据库的两种方式
+        //方式一
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+        userMapper.updateUser(new User(4,"阿狗44号","cqc112233"));
+
+        //增删改需要提交事务
+        sqlSession.commit();
+
+        //关闭
+        sqlSession.close();
+    }
+
+    @Test
+    public void test5() {
+        SqlSession sqlSession = MyBatisUtil.getSqlSession();
+
+        //操作数据库的两种方式
+        //方式一
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+        userMapper.deleteUser(4);
+
+        //增删改需要提交事务
+        sqlSession.commit();
+
+        //关闭
+        sqlSession.close();
+    }
+
+    @Test
+    public void test6() {
+        SqlSession sqlSession = MyBatisUtil.getSqlSession();
+
+        //操作数据库的两种方式
+        //方式一
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+        Map<String,Object> map = new HashMap<String,Object>();
+        map.put("uid",1);
+        List<User> users = userMapper.getUserByMap(map);
+        System.out.println(users);
+
+        //关闭
+        sqlSession.close();
+    }
+
+    @Test
+    public void test7() {
+        SqlSession sqlSession = MyBatisUtil.getSqlSession();
+
+        //操作数据库的两种方式
+        //方式一
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+
+        List<User> user = userMapper.getUserByLike("阿%");
+        System.out.println(user);
+
+        //关闭
+        sqlSession.close();
     }
 }
