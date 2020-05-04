d361904aa71221b61bb9cf7f951fe815cf2f2689
Y567 <1592221415@qq.com>
2020-05-04 18:29:08 +0800

    spring复习之复杂类型的注入

---
+++ spring-review/spring-di/src/main/java/com/gyy/pojo/Student.java
@@ -0,0 +1,18 @@
+package com.gyy.pojo;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+
+public class Student {
+
+    private String name;
+    private Address address;
+    private String[] books;
+    private List<String> hobbies;
+    private Map<String,String> gcard;
+    private Set<String> games;
+    private String wife;
+    private Properties info;
+}
