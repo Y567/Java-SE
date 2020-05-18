6953c0c126995139b1a6950779fc64a31c7f916c
Y567 <1592221415@qq.com>
2020-05-18 23:40:40 +0800

    springboot-helloWorld

---
+++ springboot-study/springboot-01-helloworld/src/main/java/com/gyy/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.gyy;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Springboot01HelloworldApplication.class);
+    }
+
+}
