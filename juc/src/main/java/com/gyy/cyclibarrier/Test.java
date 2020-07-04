bec8b424264a2b8c65b93208be1c5b7f8b7ea24f
Y567 <1592221415@qq.com>
2020-07-05 00:21:10 +0800

    执行完一定数量线程后执行指定方法CycliBarrier

---
+++ juc/src/main/java/com/gyy/cyclibarrier/Test.java
@@ -0,0 +1,25 @@
+package com.gyy.cyclibarrier;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+public class Test {
+
+    public static void main(String[] args) {
+        //七个await后就会执行函数
+        CyclicBarrier cb = new CyclicBarrier(7, () -> {
+            System.out.println("狗子你想死");
+        });
+        for (int i = 0; i < 7; i++) {
+            final int t = i;
+            new Thread(()->{
+                try {
+                    System.out.println("执行完了"+t+"任务");
+                    cb.await();
+                } catch (InterruptedException | BrokenBarrierException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+        }
+    }
+}
