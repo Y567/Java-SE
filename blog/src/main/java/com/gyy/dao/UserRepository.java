00a38700ee7d750aa2468b0e6678e4c3e1846af9
Y567 <1592221415@qq.com>
2020-07-11 16:35:53 +0800

    用户的dao层操作

---
+++ blog/src/main/java/com/gyy/dao/UserRepository.java
@@ -0,0 +1,11 @@
+package com.gyy.dao;
+
+import com.gyy.pojo.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+    //检查用户是否存在
+    User findByUsernameAndPassword(String username,String password);
+}
